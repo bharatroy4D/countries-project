543c6d3f877a53a457fadd77760b756f91cf7f3f
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-02-11 22:08:12 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+    console.log(countries)
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data));
+        
+    }, [])
+    return (
+        <div>
+            <h3>Countries:{countries.length}</h3>
+        </div>
+    );
+};
+
+export default Countries;
