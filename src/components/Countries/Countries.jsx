011522c2cf1ade7b0256a237f90f48c6adcf9244
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-02-12 11:45:49 +0600

    card of grid apply

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -14,9 +15,11 @@ const Countries = () => {
     return (
         <div>
             <h3>Countries:{countries.length}</h3>
-            {
-                countries.map(country => <Country key={country.cca3} country ={country}></Country>)
-            }
+            <div className='countries-container'>
+                {
+                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
